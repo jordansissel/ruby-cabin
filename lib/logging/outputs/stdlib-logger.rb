3b26e8e9c0905b195304bd1ebbebac55abf88e59
Jordan Sissel <jls@semicomplete.com>
2011-10-11 01:26:55 -0700

    - bite the bullet. Ruby's Logger likes strings, so emit JSON.

--- lib/logging/outputs/stdlib-logger.rb
+++ lib/logging/outputs/stdlib-logger.rb
@@ -1,7 +1,9 @@
 require "logging"
+require "json"
 
 # Wrap Ruby stdlib's logger. This allows you to output to a normal ruby logger
-# with Logging.
+# with Logging. Since Ruby's Logger has a love for strings alone, this 
+# wrapper will convert the data/event to json before sending it to Logger.
 class Logging::Outputs::StdlibLogger
   public
   def initialize(logger)
@@ -13,6 +15,6 @@ class Logging::Outputs::StdlibLogger
   def <<(data)
     method = data[:level] || "info"
     # This will call @logger.info(data) or something similar.
-    @logger.send(method, data)
+    @logger.send(method, data.to_json)
   end # def <<
 end # class Logging::Outputs::StdlibLogger
