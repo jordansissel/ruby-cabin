2637d31bbab38a5d9ce92e9e3cb42403bac05ff8
Sean Porter <portertech@gmail.com>
2011-11-07 18:46:21 -0800

    [em] ready to hand it off to whack

--- lib/cabin/outputs/em-stdlib-logger.rb
+++ lib/cabin/outputs/em-stdlib-logger.rb
@@ -2,25 +2,28 @@ require "cabin"
 require "json"
 require "eventmachine"
 
-# Wrap Ruby stdlib's logger. This allows you to output to a normal ruby logger
-# with Cabin. Since Ruby's Logger has a love for strings alone, this
-# wrapper will convert the data/event to json before sending it to Logger.
+# Wrap Ruby stdlib's logger and make it EventMachine friendly. This
+# allows you to output to a normal ruby logger with Cabin. Since
+# Ruby's Logger has a love for strings alone, this wrapper will
+# convert the data/event to json before sending it to Logger.
 class Cabin::Outputs::EmStdlibLogger
   public
   def initialize(logger)
     @logger_queue = EM::Queue.new
     @logger = logger
-    consumer # consume messages from a queue and send them to logger
+    consumer # Consume lines from a queue and send them with logger
   end # def initialize
 
   def consumer
     line_sender = Proc.new do |line|
-      # This will call @logger.info(data) or something similar.
+      # This will call @logger.info(data) or something similar
       @logger.send(line[:method], line[:message])
       EM.next_tick do
+        # Do it again
         @logger_queue.pop(&line_sender)
       end
     end
+    # Pop line off queue and send it to logger
     @logger_queue.pop(&line_sender)
   end
 
@@ -30,6 +33,7 @@ class Cabin::Outputs::EmStdlibLogger
     line = Hash.new
     line[:method] = data[:level] || "info"
     line[:message] = "#{data[:message]} #{data.to_json}"
+    # Push line onto queue for later sending
     @logger_queue.push(line)
   end # def <<
 end # class Cabin::Outputs::EmStdlibLogger
