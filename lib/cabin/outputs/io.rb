2a02a3dd02936bf80e7bed6a430062b901327e0d
Jordan Sissel <jls@semicomplete.com>
2012-03-01 16:48:27 -0800

    - add color support to io output on tty

--- lib/cabin/outputs/io.rb
+++ lib/cabin/outputs/io.rb
@@ -3,35 +3,84 @@ require "json"
 
 # Wrap IO objects with a reasonable log output. 
 #
-# If the IO instance is attached to a TTY, the output will try to be a bit more
-# human-friendly in this format:
+# If the IO is *not* attached to a tty (io#tty? returns false), then
+# the event will be written in json format terminated by a newline:
+#
+#     { "timestamp": ..., "message": message, ... }
+#
+# If the IO is attached to a TTY, there are # human-friendly in this format:
 #
 #     message {json data}
 #
-# If the IO instance is not attached to a TTY, the output will be the JSON
-# representation of the event:
-#
-#     { "timestamp": ..., "message": message, ... }
+# Additionally, colorized output may be available. If the event has :level,
+# :color, or :bold. Any of the Cabin::Mixins::Logger methods (info, error, etc)
+# will result in colored output. See the LEVELMAP for the mapping of levels
+# to colors.
 class Cabin::Outputs::IO
-  public
+  # Mapping of color/style names to ANSI control values
+  CODEMAP = {
+    :normal => 0,
+    :bold => 1,
+    :black => 30,
+    :red => 31,
+    :green => 32,
+    :yellow => 33,
+    :blue => 34,
+    :magenta => 35,
+    :cyan => 36,
+    :white => 37
+  }
+
+  # Map of log levels to colors
+  LEVELMAP = {
+    :fatal => :red,
+    :error => :red,
+    :warn => :yellow,
+    :info => nil, # default color
+    :debug => :cyan,
+  }
+
   def initialize(io)
     @io = io
   end # def initialize
 
   # Receive an event
-  public
   def <<(event)
-    if @io.tty?
-      data = event.clone
-      # delete things from the 'data' portion that's not really data.
-      data.delete(:message)
-      data.delete(:timestamp)
-      message = "#{event[:message]} #{data.to_json}"
-
-      @io.puts(message)
-      @io.flush if @io.tty?
-    else
+    if !@io.tty?
       @io.puts(event.to_json)
+    else
+      tty_write(event)
     end
   end # def <<
+
+  private
+  def tty_write(event)
+    # The io is attached to a tty, so make pretty colors.
+    # delete things from the 'data' portion that's not really data.
+    data = event.clone
+    data.delete(:message)
+    data.delete(:timestamp)
+
+    color = data.delete(:color)
+    # :bold is expected to be truthy
+    bold = data.delete(:bold) ? :bold : nil
+
+    # Make 'error' and other log levels have color
+    if color.nil? and data[:level]
+      color = LEVELMAP[data[:level]]
+    end
+
+    if data.empty?
+      message = [event[:message]]
+    else
+      message = ["#{event[:message]} #{data.to_json}"]
+    end
+    message.unshift("\e[#{CODEMAP[color.to_sym]}m") if !color.nil?
+    message.unshift("\e[#{CODEMAP[bold]}m") if !bold.nil?
+    message.push("\e[#0m") if !(bold.nil? and color.nil?)
+    @io.puts(message.join(""))
+    @io.flush
+  end # def <<
+
+  public(:initialize, :<<)
 end # class Cabin::Outputs::StdlibLogger
