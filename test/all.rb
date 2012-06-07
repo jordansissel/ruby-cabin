54ddcef9995342388e4c6c208df3112a57daca0d
Jordan Sissel <jls@semicomplete.com>
2012-06-07 11:49:16 -0700

    SKip zeromq tests because ffi-rzmq is stupid

--- test/all.rb
+++ test/all.rb
@@ -9,5 +9,9 @@ SimpleCov.start
 dir = File.dirname(File.expand_path(__FILE__))
 Dir.glob(File.join(dir, "**", "test_*.rb")).each do |path|
   puts "Loading tests from #{path}"
+  if path =~ /test_zeromq/
+    puts "Skipping zeromq tests because they force ruby to exit if libzmq is not found"
+    next
+  end
   require path
 end
